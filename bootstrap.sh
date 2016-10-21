be62b775b6ee1b67e54e003163631b3f8c8618d7
Philip Hanson <philip@cy9.org>
2016-10-21 13:57:52 -0700

    Place list of utilities directly into bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,1 @@
-sudo apt-add-repository -y ppa:cassou/emacs > /dev/null
-sudo apt-get update
-sudo apt-get install -y puppet
-
-wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.2_x86_64.deb
-sudo dpkg -i vagrant_1.5.2_x86_64.deb
-rm vagrant_1.5.2_x86_64.deb
+sudo apt install terminator chromium-browser git gitg virtualbox dia htop texlive-full jq libxml2-utils xclip emacs24 openssh-server
